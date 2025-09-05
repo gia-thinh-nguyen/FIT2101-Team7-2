4d11a0abd0214ccea0a3e67205578967ffbee725
Gia Thinh Nguyen <gngu0011@student.monash.edu>
2025-09-05 17:21:01 +1000

    feat<authentication>: redirect users based on role

--- lms-team7/app/page.tsx
+++ lms-team7/app/page.tsx
@@ -1,11 +1,19 @@
 import { auth, currentUser } from '@clerk/nextjs/server'
 import { redirect } from 'next/navigation';
+import {checkTeacher} from '@/utils/checkTeacher'
 
 export default async function Home() {
   const { userId } = await auth();
   if (!userId) {
     redirect('/sign-in');
   }
+  const isTeacher = await checkTeacher();
+    if (isTeacher) {
+      redirect('/teacher');
+    }
+    else{
+      redirect('/student');
+    }
   return (
     <></>
   );
