dc1b7a56bff65d01f0f2105c790dbfb18f7cff2f
gia-thinh-nguyen <gngu0011@student.monash.edu>
2025-09-11 13:12:54 +1000

    <refractor>: remove API routes

--- lms-team7/models/user.js
+++ lms-team7/models/user.js
@@ -3,14 +3,14 @@ const { Schema, model, models } = mongoose;
 
 const UserSchema = new Schema(
   {
+    clerkId:  { type: String, unique: true, required: true },
     name: { type: String, trim: true, required: true },
     email:     { type: String, trim: true, lowercase: true, unique: true, required: true },
     role:      { type: String, enum: ['student', 'teacher'], required: true },
 
     // optional studentProfile reference
     studentProfileId: { type: Schema.Types.ObjectId, ref: 'StudentProfile' },
-  },
-  { timestamps: true, versionKey: false }
+  }
 );
 
 
