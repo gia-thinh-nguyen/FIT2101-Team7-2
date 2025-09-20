484f6ca5fdab03259679d7a79828c94225d96688
gia-thinh-nguyen <gngu0011@student.monash.edu>
2025-09-20 10:38:20 +1000

    <fix>(auth): redirect users based on role, add admin role

--- lms-team7/utils/checkRole.ts
+++ lms-team7/utils/checkRole.ts
@@ -1,7 +1,7 @@
 import { auth } from '@clerk/nextjs/server'
 import { redirect } from 'next/navigation'
 
-export const checkTeacher = async() =>{
+export const checkRole = async(role:string) =>{
     const {sessionClaims} = await auth()
-    return sessionClaims?.metadata?.role === 'teacher'
+    return sessionClaims?.metadata?.role === role
 }
