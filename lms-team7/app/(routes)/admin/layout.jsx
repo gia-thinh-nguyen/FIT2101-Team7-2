14d2c8152c02b1e7bdf8d6d7a62818fa07056081
gia-thinh-nguyen <gngu0011@student.monash.edu>
2025-09-23 10:39:48 +1000

    fix gitignore file to ignore node_modules

--- lms-team7/app/(routes)/admin/layout.jsx
+++
@@ -1,8 +0,0 @@
-import AdminSidebar from "../../../components/admin/AdminSidebar"
-
-export default function AdminLayout({ children }) {
-  return <section>
-    <AdminSidebar />
-    {children}
-    </section>
-}
